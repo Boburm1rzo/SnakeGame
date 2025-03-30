9fbb320ab6d0b6229cee5eaa0a4c2c3b3d4c8bc9
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-03-31 02:59:14 +0500

    creating food and develop snake move

---
+++ Models/Snake.cs
@@ -0,0 +1,84 @@
+﻿using SnakeGame.ViewModels;
+
+namespace SnakeGame.Models;
+internal class Snake
+{
+    public Queue<CellVm> SnakeCells { get; } = new Queue<CellVm>();
+
+    private List<List<CellVm>> _allCells;
+    private CellVm _start;
+    private Action _generateFood;
+
+    public Snake(List<List<CellVm>> allCells, CellVm start, Action generateFood)
+    {
+        _start = start;
+        _allCells = allCells;
+        _start.CellType = CellType.Snake;
+        SnakeCells.Enqueue(start);
+        _generateFood = generateFood;
+    }
+
+    public void Restart()
+    {
+        foreach (var cell in SnakeCells)
+        {
+            cell.CellType = CellType.None;
+        }
+
+        SnakeCells.Clear();
+        _start.CellType = CellType.Snake;
+        SnakeCells.Enqueue(_start);
+    }
+
+    public void Move(MoveDirection direction)
+    {
+        var leader = SnakeCells.Last();
+
+        var nextRow = leader.Row;
+        var nextCol = leader.Column;
+
+        switch (direction)
+        {
+            case MoveDirection.Up:
+                nextRow--;
+                break;
+            case MoveDirection.Down:
+                nextRow++;
+                break;
+            case MoveDirection.Left:
+                nextCol--;
+                break;
+            case MoveDirection.Right:
+                nextCol++;
+                break;
+            default:
+                break;
+        }
+
+        try
+        {
+            var nextCell = _allCells[nextRow][nextCol];
+            switch (nextCell.CellType)
+            {
+                case CellType.None:
+                    nextCell.CellType = CellType.Snake;
+                    SnakeCells.Dequeue().CellType = CellType.None;
+                    SnakeCells.Enqueue(nextCell);
+                    break;
+                case CellType.Food:
+                    nextCell.CellType = CellType.Snake;
+                    SnakeCells.Enqueue(nextCell);
+                    _generateFood?.Invoke();
+                    break;
+                default:
+                    throw _gameOverEx;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            throw _gameOverEx;
+        }
+    }
+
+    private Exception _gameOverEx => throw new Exception("Game over");
+}
--- ViewModels/MainWndVM.cs
+++ ViewModels/MainWndVM.cs
@@ -1,4 +1,6 @@
 ﻿using SnakeGame.Models;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SnakeGame.ViewModels;
 internal class MainWndVM : BindableBase
@@ -12,6 +14,11 @@ internal class MainWndVM : BindableBase
         {
             _continueGame = value;
             RaisePropertyChanged(nameof(ContinueGame));
+
+            if (_continueGame)
+            {
+                SnakeGo();
+            }
         }
     }
 
@@ -22,9 +29,16 @@ internal class MainWndVM : BindableBase
 
     private int _rowCount = 10;
     private int _columnCount = 10;
+    private const int SNAKESPEED = 400;
+    private int _snakeSpeed = SNAKESPEED;
 
-    public MainWndVM()
+    private MainWindow _mainWindow;
+    private Snake _snake;
+    private CellVm _lastFood;
+
+    public MainWndVM(MainWindow mainWindow)
     {
+        _mainWindow = mainWindow;
         StartStopCommand = new DelegateCommand(() => ContinueGame = !ContinueGame);
 
         for (int row = 0; row < _rowCount; row++)
@@ -33,10 +47,97 @@ internal class MainWndVM : BindableBase
             for (int col = 0; col < _columnCount; col++)
             {
                 var cell = new CellVm(row, col);
+                rowList.Add(cell);
             }
 
             AllCells.Add(rowList);
         }
+
+        _snake = new Snake(AllCells, AllCells[_rowCount / 2][_columnCount / 2], CreateFood);
+        CreateFood();
+
+        _mainWindow.KeyDown += UserKeyDown;
     }
 
+    private async Task SnakeGo()
+    {
+        while (ContinueGame)
+        {
+            await Task.Delay(_snakeSpeed);
+
+            try
+            {
+                _snake.Move(_currentMoveDirection);
+
+            }
+            catch (Exception ex)
+            {
+                ContinueGame = false;
+                MessageBox.Show(ex.Message);
+                _snakeSpeed = SNAKESPEED;
+                _snake.Restart();
+                _lastFood.CellType = CellType.None;
+                CreateFood();
+            }
+        }
+    }
+
+    private void UserKeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.A:
+                if (_currentMoveDirection != MoveDirection.Right)
+                    _currentMoveDirection = MoveDirection.Left;
+                break;
+            case Key.S:
+                if (_currentMoveDirection != MoveDirection.Up)
+                    _currentMoveDirection = MoveDirection.Down;
+                break;
+            case Key.D:
+                if (_currentMoveDirection != MoveDirection.Left)
+                    _currentMoveDirection = MoveDirection.Right;
+                break;
+            case Key.W:
+                if (_currentMoveDirection != MoveDirection.Down)
+                    _currentMoveDirection = MoveDirection.Up;
+                break;
+            case Key.Left:
+                if (_currentMoveDirection != MoveDirection.Right)
+                    _currentMoveDirection = MoveDirection.Left;
+                break;
+            case Key.Right:
+                if (_currentMoveDirection != MoveDirection.Left)
+                    _currentMoveDirection = MoveDirection.Right;
+                break;
+            case Key.Up:
+                if (_currentMoveDirection != MoveDirection.Down)
+                    _currentMoveDirection = MoveDirection.Up;
+                break;
+            case Key.Down:
+                if (_currentMoveDirection != MoveDirection.Up)
+                    _currentMoveDirection = MoveDirection.Down;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CreateFood()
+    {
+        var random = new Random();
+
+        var row = random.Next(0, _rowCount);
+        var col = random.Next(0, _columnCount);
+
+        _lastFood = AllCells[row][col];
+
+        if (_snake.SnakeCells.Contains(_lastFood))
+        {
+            CreateFood();
+        }
+
+        _lastFood.CellType = CellType.Food;
+        _snakeSpeed = (int)(_snakeSpeed * 0.95);
+    }
 }
